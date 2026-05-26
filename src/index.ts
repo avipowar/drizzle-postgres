fd438865d48b59fc30a169e2666f453551eb82c7
avipowar <powaravinash06/2gmail.com>
2026-05-26 18:16:40 +0530

    create http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("hii a vainshsh");
+import { createServer } from "node:http";
+
+function main() {
+  try {
+    const server = createServer();
+    const PORT: number = 8080;
+
+    server.listen(PORT, () => {
+      console.log(`Http server is runnig on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`error starting http server`);
+    throw error;
+  }
+}
+
+main();
